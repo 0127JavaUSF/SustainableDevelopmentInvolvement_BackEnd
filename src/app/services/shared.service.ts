c2fb8fedd24201bf440c51f7356b9ab23fa1f403
Henry Salazar <hello369@live.com>
2020-03-04 18:28:02 -0500

    search component

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  public types : String[] = ["Dog", "Cat", "Bird", "Rodent", "Fish", "Reptile", "Amphibian"];
+
+  constructor() { }
+
+  //this converts the type String to the Number value used in Java
+  public typeToNumber(type : String) : Number {
+
+    for(let i = 0; i < this.types.length; i++) {
+
+      if(type === this.types[i]) {
+        return i + 1;
+      }
+    }
+    return 0;
+  }
+}
