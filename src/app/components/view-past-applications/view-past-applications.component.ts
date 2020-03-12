b658c4b7a5e821d529463fc5d5bd9eec2100d525
Daniel <chateaustdanielle@gmail.com>
2020-03-12 13:09:32 -0400

    Seventh Commit

--- src/app/components/view-past-applications/view-past-applications.component.ts
+++ src/app/components/view-past-applications/view-past-applications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { Router } from '@angular/router';
+import { ApplicationService } from 'src/app/services/application.service';
 
 @Component({
   selector: 'app-view-past-applications',
@@ -11,77 +13,18 @@ export class ViewPastApplicationsComponent implements OnInit {
   selectedType : String;
   test : String = "Search Works";
 
-  results : [
-    {
-      id : Number,
-      name : String,
-      age : Number,
-      sex : Number,
-      species : String,
-      city : String,
-      state : String,
-      zipCode : String,
-      imageUrls : String[],
-      fixed : Boolean,
-      status: String,
-      date: String
-  },
-  {
-    id : Number,
-    name : String,
-    age : Number,
-    sex : Number,
-    species : String,
-    city : String,
-    state : String,
-    zipCode : String,
-    imageUrls : String[],
-    fixed : Boolean,
-    status: String,
-    date: String
-  }
-  ];
-
-  constructor(private shared: SharedService) { }
+  results : any;
+  constructor(private shared: SharedService, private route : Router, private applicationService : ApplicationService) { }
 
   ngOnInit() {
+    this.applicationService.getPastApplication().subscribe(data => {
+      this.results = data;
+    }, error => {
+    });
 
     //do post request to get search results
 
-    //fake data from post request
-    this.results = [
-      {
-        id : 1,
-        name : 'Fido',
-        age : 0,
-        sex : 1,
-        species: 'Pitbull',
-        city: 'Tampa',
-        state: 'FL',
-        zipCode: '32720',
-        imageUrls : ['m1_0.png', 'm2_0.png'],
-        fixed : true,
-        status: 'pending',
-        date:'today'
-
-      },
-      {
-        id : 2,
-        name : 'Spot',
-        age : 1,
-        sex : 0,
-        species: '',
-        city: 'Tampa',
-        state: 'FL',
-        zipCode: '32720',
-        imageUrls : ['m2_0.png', 'm1_0.png'],
-        fixed : true,
-        status: 'accepted',
-        date:'yesterday'
-      }
-    ];
   }
-
   onDetailsClicked(event, result) {
 
     //get listing id
